68ee30af1f7746e5286297924c9787ef14aac93c
linhDevIOS <linhappcare@gmail.com>
2019-08-23 12:58:45 +0700

    add 2 new item

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("add new code to gith")
         print("new item")
+        print("new item")
         // Do any additional setup after loading the view.
     }
 
