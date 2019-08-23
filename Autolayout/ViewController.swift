208ebdd61d44cc727cab88419dcdbb31b99f6e88
hoanglinh <hoanglinh6794@gmail.com>
2019-08-23 12:41:12 +0700

    add new item

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("add new code to gith")
+        print("new item")
         // Do any additional setup after loading the view.
     }
 
